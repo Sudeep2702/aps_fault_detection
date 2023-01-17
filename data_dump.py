378426a20eed04da6ee11f910d23c9afc39d34ff
sudeep2702 <sujaynyasargi4@gmail.com>
2023-01-17 14:18:59 +0530

    created space in data_dump.py file

--- data_dump.py
+++ data_dump.py
@@ -5,7 +5,7 @@ import json
 Data_file_path ='/config/workspace/aps_failure_training_set1.csv'
 client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
 Data_base_name = 'aps'
-collection_name = 'sensor'
+collection_name =  'sensor'
 if __name__=='__main__':
     df = pd.read_csv(Data_file_path)
     print(f"Rows and columns :{df.shape}")
